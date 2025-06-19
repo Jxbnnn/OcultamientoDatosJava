148daf13b3f5b97185d4d2e3cfd71081d884ec17
Benjamin <benjaminurrea14@gmail.com>
2025-06-19 03:49:00 -0400

    imports

--- src/main/java/org/example/Main.java
+++
@@ -1,4 +0,0 @@
-package org.example;
-
-import java.util.Calendar;
-import java.util.GregorianCalendar;
---
+++ src/main/java/org/example/TodaysDate.java
@@ -0,0 +1,8 @@
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+public class TodaysDate {
+    String time;
+    public int day;
+    private int month;
+    protected int year;
